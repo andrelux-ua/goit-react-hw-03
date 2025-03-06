e0563c40f1f51d0735af7534dea1c649fd7ed6e2
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-03-06 20:59:15 +0200

    create-hw-react-03

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </div>
+  );
+}
+
+export default App;
